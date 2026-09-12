9d9798a7c7d51d5e4ff2d657cfa4ef7fcce02adc
SeptiaRamadhaniPutriImiyanti <dwirositasalsabilla0@gmail.com>
2026-09-12 07:51:48 -0700

    feat: tambah informasi lokasi UMKM

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const promoButton = document.querySelector("#promoButton");
 
 promoButton.addEventListener("click", () => {
-  promoButton.textContent = "Promo: Beli 2 gratis tester!";
+  promoButton.textContent = "Promo: Kopi Susu Gula Aren Rp18.000!";
   console.log("Promo Kopi Nusa berhasil ditampilkan.");
 });
